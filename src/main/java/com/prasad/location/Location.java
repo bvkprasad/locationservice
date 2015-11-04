ab6f4dd2afe8c768b6bb52e704d03354d395d7cd
bvkprasad <prasadbulusu9@hotmail.com>
2015-11-04 21:07:56 +0530

    locationservice35

--- src/main/java/com/prasad/location/Location.java
+++ src/main/java/com/prasad/location/Location.java
@@ -34,7 +34,7 @@ public class Location {
 			rs = ps.executeQuery();
 			boolean check = true;
 			while(rs.next()){
-				String obj = "{lat:"+rs.getDouble(1)+",lng:"+rs.getDouble(2)+"}";
+				String obj = "{\"lat\":"+rs.getDouble(1)+",\"lng\":"+rs.getDouble(2)+"}";
 				if(check){
 					json = obj;
 					check = false;
